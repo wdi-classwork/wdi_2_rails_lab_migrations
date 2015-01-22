ba92853d5d52c8123633b2c93b0649e93a86c2c9
Helen Thi <ht5658@gmail.com>
2015-01-22 17:42:37 -0500

    step 2 complete

--- db/migrate/20150122223831_add_city_and_state_to_locations.rb
+++ db/migrate/20150122223831_add_city_and_state_to_locations.rb
@@ -1,5 +1,6 @@
 class AddCityAndStateToLocations < ActiveRecord::Migration
   def change
     add_column :locations, :city, :text
+    add_column :locations, :state, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122222653) do
+ActiveRecord::Schema.define(version: 20150122223831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150122222653) do
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "city"
+    t.text     "state"
   end
 
 end
