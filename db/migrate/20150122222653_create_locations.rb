6057084e84c311ade66a48a1e36f7eec440fddb4
Helen Thi <ht5658@gmail.com>
2015-01-22 17:38:11 -0500

    step 1

--- db/migrate/20150122222653_create_locations.rb
+++ db/migrate/20150122222653_create_locations.rb
@@ -1,6 +1,10 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
+      t.float :lat
+      t.float :long
+      t.text :name
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122220137) do
+ActiveRecord::Schema.define(version: 20150122222653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "locations", force: :cascade do |t|
+    t.float    "lat"
+    t.float    "long"
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
